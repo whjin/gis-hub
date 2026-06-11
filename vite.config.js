f89bcb9e9ec30a19288f5b66d6315a04cd6968b2
whjin <wuhuajin09@163.com>
2026-06-12 07:15:14 +0800

    提交更新

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
+import { fileUrlToPath } from 'url';
 
-export default defineConfig({
-  base: '/',
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/gis-hub/' : './',
+    resolve: {
+      alias: {
+        '@': fileUrlToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    build: {
+      chunkSizeWarningLimit: 1000,
+      emptyOutDir: true,
+      outDir: 'dist',
+    },
+  };
 });
